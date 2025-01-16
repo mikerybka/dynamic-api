416a5072a814fbaf951626efadefbc0b1c09d321
Mike Rybka <merybka@gmail.com>
2025-01-16 11:27:53 -0500

    Initial implementation

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mikerybka/dynamic-app
+
+go 1.23.4
+
+require github.com/mikerybka/brass v0.0.0-20250116161908-9ff495ff5d32
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/mikerybka/brass"
+)
+
+func requireEnvVar(name string) string {
+	v := os.Getenv(name)
+	if v == "" {
+		fmt.Println(name, "required")
+		os.Exit(1)
+	}
+	return v
+}
+
+func main() {
+	app := &brass.App{
+		DataDir: requireEnvVar("DATA_DIR"),
+		SrcDir:  requireEnvVar("SRC_DIR"),
+	}
+	addr := ":" + requireEnvVar("PORT")
+	err := http.ListenAndServe(addr, app)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
